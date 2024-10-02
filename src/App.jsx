43500fdab5f38bd4e11054b155665ddce3173620
LanHuong111 <lelanhuong2k4@gmail.com>
2024-10-02 19:50:24 +0700

    master

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Footer from './component/Footer'
+import Header from './component/Header'
+import HomePage from './pages/HomePage'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <HomePage/>
+      <Footer/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Content from "../component/Content";
+import HomeSection from "../component/HomeSection";
+import BookingForm from "../component/BookingForm";
+
+function HomePage() {
+  return (
+    <>
+      <HomeSection />
+      <BookingForm />
+      <Content />
+    </>
+  );
+}
+
+export default HomePage;
