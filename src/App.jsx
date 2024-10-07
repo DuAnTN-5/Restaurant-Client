cfd23114fe3df9661aa56adf2e16c6ffd89ce361
LanHuong111 <lelanhuong2k4@gmail.com>
2024-10-07 18:14:26 +0700

    LanHuong ContactPage

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
       <Header />
 
       {/* <HomePage/> */}
-      {/* <Contact/> */}
-      <CartProduct/>
+      <Contact/>
+      {/* <CartProduct/> */}
       {/* <ProductDetail/> */}
       {/* <Blog/> */}
       {/* <FavoritePage/> */}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,53 +2,18 @@ import "../css/Contact.css";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdPhonePortrait } from "react-icons/io";
 import { IoMailOpen } from "react-icons/io5";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillTikTok } from "react-icons/ai";
+import { FaTelegram } from "react-icons/fa";
+
 
 function Contact() {
   return (
     <>
       <div className="contact">
-        <div className="contact-item">
-          <div className="contact-map">
-            <iframe
-              id="map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.918237254525!2d105.81494031536003!3d21.028511393148378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abb5c03bc50f%3A0x4cb490e3b6e6dd7b!2zSOG7kyBIw6AgSMOgIFjhuq1uZyBIw6A!5e0!3m2!1svi!2s!4v1614854344721!5m2!1svi!2s"
-              // style={{ width: "100%", height: "500px", border: "2px solid black", borderRadius: "10px" }}
-              allowFullScreen=""
-              loading="lazy"
-            />
-          </div>
-          <div className="contact__header">
-            <div className="contact-info">
-              <i className="contact-icon">
-                <FaLocationDot />
-              </i>
-              <span className="contact-text">Cao đẳng FPT Polytechnic</span>
-            </div>
-            <a className="contact-text-item"> Rosemead, CA 91770 </a>
-            <div className="contact-info">
-              <i className="contact-icon">
-                <IoMdPhonePortrait />
-              </i>
-              <span className="contact-text">079 79 79 79</span>
-            </div>
-            <a className="contact-text-item"> Mon to Fri 9am to 6pm </a>
-            <div className="contact-info">
-              <i className="contact-icon">
-                <IoMailOpen />
-              </i>
-              <span className="contact-text">
-                HightFiverestaurant@gmail.com
-              </span>
-            </div>
-            <a className="contact-text-item"> Send us your query anytime!!</a>
-            {/* <div>
-                <h3>NHÀ HÀNG HIGHTFIVE</h3>
-                <span>Chỉnh chu cho từng khâu chuẩn bị!!</span>
-
-              </div> */}
-          </div>
-        </div>
-        <div className="form-container">
+      <div className="form-container">
           <h3 className="form-title">Liên hệ với chúng tôi</h3>
           <h4 className="form-description">
             {" "}
@@ -59,11 +24,11 @@ function Contact() {
           <div className="contact-form">
             <div className="form-input">
               <div>
-                <label htmlFor="email">Email</label>
+                <label htmlFor="name">Họ và tên</label>
                 <input
                   type="text"
-                  id="email"
-                  placeholder="Nhập email của bạn"
+                  id="name"
+                  placeholder="Nhập tên của bạn"
                 />
               </div>
               <div>
@@ -90,7 +55,51 @@ function Contact() {
             </div>
           </div>
         </div>
+        <div className="contact__header">
+            <div className="contact-info">
+              <i className="contact-icon">
+                <FaLocationDot />
+              </i>
+              <span className="contact-text">Cao đẳng FPT Polytechnic</span>
+            </div>
+            <a className="contact-text-item"> Rosemead, CA 91770 </a>
+            <div className="contact-info">
+              <i className="contact-icon">
+                <IoMdPhonePortrait />
+              </i>
+              <span className="contact-text">079 79 79 79</span>
+            </div>
+            <a className="contact-text-item"> Mon to Fri 9am to 6pm </a>
+            <div className="contact-info">
+              <i className="contact-icon">
+                <IoMailOpen />
+              </i>
+              <span className="contact-text">
+                HightFiverestaurant@gmail.com
+              </span>
+            </div>
+            <a className="contact-text-item"> Send us your query anytime!!</a>
+            <div className="contact-icon-website">
+              <i><FaSquareInstagram/></i>
+              <i><FaFacebook/></i>
+              <i><FcGoogle/></i>
+              <i><AiFillTikTok/></i>
+              <i><FaTelegram/></i>
+            </div>
+          </div>
+        
       </div>
+        <div className="contact-item">
+          <div className="contact-map">
+            <iframe
+              id="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.918237254525!2d105.81494031536003!3d21.028511393148378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abb5c03bc50f%3A0x4cb490e3b6e6dd7b!2zSOG7kyBIw6AgSMOgIFjhuq1uZyBIw6A!5e0!3m2!1svi!2s!4v1614854344721!5m2!1svi!2s"
+              // style={{ width: "100%", height: "500px", border: "2px solid black", borderRadius: "10px" }}
+              allowFullScreen=""
+              loading="lazy"
+            />
+          </div>
+        </div>
     </>
   );
 }
